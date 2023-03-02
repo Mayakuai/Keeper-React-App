b2a3699ea614f4af6d528f69065d4226c4c8e0a9
Joshua Stuart <62312697+Mayakuai@users.noreply.github.com>
2023-03-02 15:53:41 -0500

    adding props and other small changes adding old CSB to git hub

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,19 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "../notes";
+
+function createEntry(cardTerm) {
+  return (
+    <Note key={cardTerm.id} title={cardTerm.title} content={cardTerm.content} />
+  );
+}
 
 function App() {
   return (
     <div>
       <Header />
-      <Note />
+      {notes.map(createEntry)}
       <Footer />
     </div>
   );
